118d0ee21e52d8eb064fd7a8479de4d68a6d2362
Varnasus <zach.varney@eyeverify.com>
2017-02-27 09:13:26 -0600

    added a few recursive functions

--- FeelingGood/app/src/main/java/com/example/android/feelinggood/MainActivity.java
+++ FeelingGood/app/src/main/java/com/example/android/feelinggood/MainActivity.java
@@ -10,4 +10,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public static int summation (int n) {
+        if (n<=0) {
+            return 0;
+        }
+        else {
+            System.out.println("sn: " + n);
+            return n + summation(n -1);
+        }
+    }
+
+    public static int factorial(int n) {
+        if (n<=1) {
+            System.out.println("fn: " + n);
+            return 1;
+        }
+        else {
+            System.out.println("fn: " + n);
+            return n * factorial(n-1);
+        }
+
+    }
+
+    public static int exponentation(int n, int p) {
+        if (p<=0) {
+            return 1;
+        }
+        else {
+            System.out.println("En: " + n);
+            System.out.println("Ep: " + p);
+            return n * exponentation(n, p-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        summation(3);
+        factorial(3);
+        exponentation(5, 3);
+    }
 }
