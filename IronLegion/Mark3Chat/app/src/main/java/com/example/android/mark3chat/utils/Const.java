95f6f9def074c33dd8164d6ac2af83d67b326822
JARVIS\Varnasus <com.gmail.zvarney>
2017-08-30 23:59:50 -0500

    Initial Commit - Mark 3

---
+++ IronLegion/Mark3Chat/app/src/main/java/com/example/android/mark3chat/utils/Const.java
@@ -0,0 +1,16 @@
+package com.example.android.mark3chat.utils;
+
+/**
+ * Created by Zachary on 8/30/2017.
+ */
+
+
+/**
+ * The Class Const is a single common class to hold all the app Constants.
+ */
+public class Const
+{
+
+    /** The Constant EXTRA_DATA. */
+    public static final String EXTRA_DATA = "extraData";
+}
