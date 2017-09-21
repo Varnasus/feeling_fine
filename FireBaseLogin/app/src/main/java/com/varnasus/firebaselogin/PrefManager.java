94fb8e59736351ee0604d341f446f2055a635e6a
JARVIS\Varnasus <com.gmail.zvarney>
2017-09-21 14:51:27 -0500

    Added Welcome screens

---
+++ FireBaseLogin/app/src/main/java/com/varnasus/firebaselogin/PrefManager.java
@@ -0,0 +1,40 @@
+package com.varnasus.firebaselogin;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Zachary on 9/21/2017.
+ *
+ * This is used for the Welcome screen as of 9/21/17
+ */
+
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "androidhive-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+}
