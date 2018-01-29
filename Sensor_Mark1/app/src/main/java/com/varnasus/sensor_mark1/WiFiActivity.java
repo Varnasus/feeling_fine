f08d1291ae41c101d7e4f12ff39061146a5d46e2
JARVIS\Varnasus <com.gmail.zvarney>
2018-01-29 14:38:41 -0600

    -Added bssid to WiFi Activity

--- Sensor_Mark1/app/src/main/java/com/varnasus/sensor_mark1/WiFiActivity.java
+++ Sensor_Mark1/app/src/main/java/com/varnasus/sensor_mark1/WiFiActivity.java
@@ -5,43 +5,52 @@ import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import java.util.List;
+
 public class WiFiActivity extends Activity {
 
-    private static final String TAG = "VARNEY: WiFiActivity";
+    private static final String TAG = "VARNEY: WiFiActivity - ";
 
     ToggleButton toggleWiFi;
     TextView wifiName;
-    TextView textView;
+    TextView enableWifiTv;
+    TextView bssidTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wi_fi);
 
+        Log.v(TAG, "onCreate");
+
         wifiName = findViewById(R.id.wifi_name);
-        textView = findViewById(R.id.textView);
+        enableWifiTv = findViewById(R.id.enable_wifi_tv);
+        bssidTv = findViewById(R.id.bssid_name);
 
         toggleWiFi = findViewById(R.id.wifi_switch);
 
         getCurrentSsid(this);
+        getCurrentBssid(bssidTv);
 
         toggleWiFi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
                 if (checked) {
-                    textView.setText("WiFi is ON");
+                    enableWifiTv.setText("WiFi is ON");
                     WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
                     wifi.setWifiEnabled(true);
                 } else {
-                    textView.setText("WiFi is OFF");
+                    enableWifiTv.setText("WiFi is OFF");
                     WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
                     wifi.setWifiEnabled(false);
                 }
@@ -49,13 +58,13 @@ public class WiFiActivity extends Activity {
         });
 
         if (toggleWiFi.isChecked()) {
-            textView.setText("WiFi is ON");
+            enableWifiTv.setText("WiFi is ON");
             WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
             wifi.setWifiEnabled(true);
         } else {
-            textView.setText("WiFi is OFF");
+            enableWifiTv.setText("WiFi is OFF");
             WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-            wifi.setWifiEnabled(false);
+//            wifi.setWifiEnabled(false);
         }
     }
 
@@ -77,4 +86,38 @@ public class WiFiActivity extends Activity {
         return ssid;
     }
 
+    public void getCurrentBssid (TextView tvWifiList) {
+        WifiManager wifiManager = (WifiManager)
+                getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        List<ScanResult> results = wifiManager.getScanResults();
+        String message = "No results. Check wireless is on";
+
+        if (results != null) {
+
+            final int size = results.size();
+
+            if (size == 0) {
+                message = "No access points in range";
+            } else {
+                ScanResult bestSignal = results.get(0);
+                tvWifiList.setText(""); // etWifiList is EditText
+                int count = 1;
+                for (ScanResult result : results) {
+                    tvWifiList.append(count++ + ". " + result.SSID + " : "
+                            + result.level + "\n" + result.BSSID + "\n"
+                            + result.capabilities + "\n"
+                            + "\n=======================\n");
+                    if (WifiManager.compareSignalLevel(bestSignal.level,
+                            result.level) < 0) {
+                        bestSignal = result;
+                    }
+                }
+                message = String.format(
+                        "%s networks found. %s is the strongest.", size,
+                        bestSignal.SSID + " : " + bestSignal.level);
+            }
+        }
+        bssidTv.setText(message);
+//        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
 }
